ec7027b7afd53e124bc52458437cca3f000e0dc0
baidala <stavr@bk.ru>
2016-02-10 19:31:15 +0200

    Update HibernateUtil.java

--- HibernateUtil.java
+++ HibernateUtil.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package transport;
 
 import java.io.File;
@@ -18,11 +13,9 @@ public class HibernateUtil {
     private static final SessionFactory sessionFactory = buildSessionFactory();
     
     private static SessionFactory buildSessionFactory() {
-        try {       
-        return new Configuration().configure(new File("hibernate.cfg.xml")).buildSessionFactory();
-         } catch (Throwable ex) {
-            throw new ExceptionInInitializerError(ex);
-      }
+              
+        return new Configuration().configure(new File("src\\transport\\hibernate.cfg.xml")).buildSessionFactory();
+         
     }
     
     
